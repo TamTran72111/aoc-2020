1faa233cbe77294916acc785a09abec5c45e1e5d
TamTran72111 <tam.tran72111@gmail.com>
2020-12-10 12:24:15 -0500

    Implement day 3 in Rust

---
+++ rust/src/day3.rs
@@ -0,0 +1,44 @@
+use crate::utilities::read_lines;
+
+fn count_trees(data: &Vec<Vec<char>>, move_: &(usize, usize)) -> i32 {
+    let mut trees = 0;
+    let mut row = 0;
+    let mut col = 0;
+
+    while row < data.len() {
+        if data[row][col] == '#' {
+            trees += 1;
+        }
+        row += move_.1;
+        col += move_.0;
+        col %= data[0].len();
+    }
+
+    trees
+}
+
+fn part_1(data: &Vec<Vec<char>>) {
+    let move_ = (3, 1);
+    println!("\tPart 1: {}", count_trees(data, &move_));
+}
+
+fn part_2(data: &Vec<Vec<char>>) {
+    let moves = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    println!(
+        "\tPart 1: {}",
+        moves
+            .iter()
+            .map(|move_| count_trees(data, move_))
+            .fold(1, |x, y| x * y)
+    );
+}
+
+pub fn main() {
+    println!("Day 3");
+    let data: Vec<Vec<char>> = read_lines("../inputs/day3.txt")
+        .into_iter()
+        .map(|line| line.chars().collect())
+        .collect();
+    part_1(&data);
+    part_2(&data);
+}
