01018ecbaabdb4f8ab85fec3eb5a6a0f4cf8cc7f
TamTran72111 <tam.tran72111@gmail.com>
2020-12-20 19:54:14 -0500

    Commit changes in Python utilities module

--- python/utilities.py
+++ python/utilities.py
@@ -11,3 +11,7 @@ def read_lines(file_name):
 def read_int_input(file_name):
     data = read_lines(file_name)
     return list(map(int, data))
+
+
+def read_blocks(file_name):
+    return read_input(file_name).split('\n\n')
